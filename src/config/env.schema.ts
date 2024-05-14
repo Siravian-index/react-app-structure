86585f64ff47d401248684b272ef0f32a045ad36
David Chavez <depch47@gmail.com>
2024-05-14 10:55:31 -0500

    Update env variables and schema

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -2,5 +2,7 @@ import z from "zod"
 
 
 export const schema = z.object({
-  VITE_BASE_API_URL: z.string()
+  VITE_BASE_API_URL: z.string(),
+  VITE_GOOGLE_CLIENT_ID: z.string(),
+  VITE_GOOGLE_OAUTH_REDIRECT_URL: z.string(),
 })
